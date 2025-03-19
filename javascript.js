0a5ae19765376274f331c17a8b6a7b7a7d26b5b3
marodacco <48792073+marodacco@users.noreply.github.com>
2025-03-19 10:43:47 +0900

    Create getComputerChoice function
    Create a function to create a randum number
    and change it into rock, paper or scissors
    and return it.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,19 @@
-console.log("Hello World")
+function getComputerChoice() {
+  // Get a random number between 0 to 3
+  const randomNumber = Math.floor(Math.random() * 3);
+  
+  // IF the number is 0, return rock
+  // ELSE IF the number is 1, return paper
+  // ELSE return scissors
+  if (randomNumber === 0) {
+    return "rock";
+  }
+  else if (randomNumber === 1) {
+    return "paper";
+  }
+  else {
+    return "scissors";
+  }
+}
+
+console.log(getComputerChoice());
